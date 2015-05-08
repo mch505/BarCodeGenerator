c6f2c53fbcfde37cf09d448660c53198358d9569
mch505 <mariocheves05@gmail.com>
2015-05-08 11:39:51 -0600

    BarCode Generator Ready to use

---
+++ BarCodeGenerator/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace BarCodeGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pic_barcode = new System.Windows.Forms.PictureBox();
+            this.txt_content = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_generate = new System.Windows.Forms.Button();
+            this.btn_save_as = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_barcode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Pic_barcode
+            // 
+            this.Pic_barcode.Location = new System.Drawing.Point(322, 9);
+            this.Pic_barcode.Name = "Pic_barcode";
+            this.Pic_barcode.Size = new System.Drawing.Size(309, 185);
+            this.Pic_barcode.TabIndex = 0;
+            this.Pic_barcode.TabStop = false;
+            // 
+            // txt_content
+            // 
+            this.txt_content.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_content.Location = new System.Drawing.Point(23, 28);
+            this.txt_content.Name = "txt_content";
+            this.txt_content.Size = new System.Drawing.Size(293, 22);
+            this.txt_content.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Write your code here";
+            // 
+            // btn_generate
+            // 
+            this.btn_generate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_generate.Location = new System.Drawing.Point(23, 54);
+            this.btn_generate.Name = "btn_generate";
+            this.btn_generate.Size = new System.Drawing.Size(149, 33);
+            this.btn_generate.TabIndex = 3;
+            this.btn_generate.Text = "Generate BarCode";
+            this.btn_generate.UseVisualStyleBackColor = true;
+            this.btn_generate.Click += new System.EventHandler(this.btn_generate_Click);
+            // 
+            // btn_save_as
+            // 
+            this.btn_save_as.Enabled = false;
+            this.btn_save_as.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_save_as.Location = new System.Drawing.Point(179, 54);
+            this.btn_save_as.Name = "btn_save_as";
+            this.btn_save_as.Size = new System.Drawing.Size(137, 33);
+            this.btn_save_as.TabIndex = 4;
+            this.btn_save_as.Text = "Save As ..";
+            this.btn_save_as.UseVisualStyleBackColor = true;
+            this.btn_save_as.Click += new System.EventHandler(this.btn_save_as_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(23, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 33);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "New Code";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 206);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_save_as);
+            this.Controls.Add(this.btn_generate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_content);
+            this.Controls.Add(this.Pic_barcode);
+            this.Name = "Form1";
+            this.Text = "Bar Code Generator Example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Pic_barcode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Pic_barcode;
+        private System.Windows.Forms.TextBox txt_content;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_generate;
+        private System.Windows.Forms.Button btn_save_as;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BarCodeGenerator/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Spire.Barcode.Forms;
+using ZXing;
+
+namespace BarCodeGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+        private void GenerateBarCode(string content)
+        {
+            //Create the Barcode Writer instance
+            var writer = new BarcodeWriter
+            {
+                //sets the barcode formate is CODE 128 just for the example
+                Format = BarcodeFormat.CODE_128
+            };
+            //writes the content of the barcod
+            var bitmap = writer.Write(content);
+            //shows the barcode in the form
+            Pic_barcode.Image = bitmap;
+            
+        }
+
+        private void btn_generate_Click(object sender, EventArgs e)
+        {
+            if (txt_content.Text != string.Empty)
+            {
+                GenerateBarCode(txt_content.Text);
+                btn_generate.Enabled = false;
+                btn_save_as.Enabled = true;
+                txt_content.Enabled = false;
+            }
+            else
+                MessageBox.Show(this, "Please write the content of your barcode", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CleanForm();
+        }
+        private void CleanForm()
+        {
+            Pic_barcode.Image = null;
+            txt_content.Text = "";
+            btn_save_as.Enabled = false;
+            btn_generate.Enabled=txt_content.Enabled = true;
+            txt_content.Focus();
+        }
+        private void SaveBarcodeAsImage()
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif";
+            save.Title = "Save the Barcode as Image File";
+            save.ShowDialog();
+
+            // If the file name is not an empty string open it for saving.
+            if (save.FileName != "")
+            {
+                // Saves the Image via a FileStream created by the OpenFile method.
+                System.IO.FileStream fs =
+                   (System.IO.FileStream)save.OpenFile();
+                // Saves the Image in the appropriate ImageFormat based upon the
+                // File type selected in the dialog box.
+                switch (save.FilterIndex)
+                {
+                    case 1:
+                        this.Pic_barcode.Image.Save(fs,
+                           System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+
+                    case 2:
+                        this.Pic_barcode.Image.Save(fs,
+                           System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+
+                    case 3:
+                        this.Pic_barcode.Image.Save(fs,
+                           System.Drawing.Imaging.ImageFormat.Gif);
+                        break;
+                }
+
+                fs.Close();
+            }
+        }
+        private void btn_save_as_Click(object sender, EventArgs e)
+        {
+            SaveBarcodeAsImage();
+        }
+        
+    }
+}
